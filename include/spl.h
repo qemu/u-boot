100f0728f6c7748f45fd20f578699badb67fcb78
Peng Fan <peng.fan@nxp.com>
2019-08-07 06:40:53 +0000

    spl: pass args to board_return_to_bootrom
    Pass spl_image and bootdev to board_return_bootrom.
    i.MX8MN needs the args to let ROM to load images
    
    Cc: Simon Glass <sjg@chromium.org>
    Cc: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
    Cc: Kever Yang <kever.yang@rock-chips.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>

--- include/spl.h
+++ include/spl.h
@@ -382,7 +382,8 @@ void spl_optee_entry(void *arg0, void *arg1, void *arg2, void *arg3);
  * stage wants to return to the ROM code to continue booting, boards
  * can implement 'board_return_to_bootrom'.
  */
-void board_return_to_bootrom(void);
+int board_return_to_bootrom(struct spl_image_info *spl_image,
+			    struct spl_boot_device *bootdev);
 
 /**
  * board_spl_fit_post_load - allow process images after loading finished
