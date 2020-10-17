0cc2e45ed8ee54573ce6db7f7b74761d889b7885
Sean Anderson <seanga2@gmail.com>
2020-10-17 14:07:28 -0400

    log: Add additional const qualifier to arrays
    Both these arrays and their members are const. Fixes checkpatch complaint.
    
    Signed-off-by: Sean Anderson <seanga2@gmail.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- common/log.c
+++ common/log.c
@@ -13,7 +13,7 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
-static const char *log_cat_name[LOGC_COUNT - LOGC_NONE] = {
+static const char *const log_cat_name[LOGC_COUNT - LOGC_NONE] = {
 	"none",
 	"arch",
 	"board",
@@ -28,7 +28,7 @@ static const char *log_cat_name[LOGC_COUNT - LOGC_NONE] = {
 	"acpi",
 };
 
-static const char *log_level_name[LOGL_COUNT] = {
+static const char *const log_level_name[LOGL_COUNT] = {
 	"EMERG",
 	"ALERT",
 	"CRIT",
