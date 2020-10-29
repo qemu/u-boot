70f8edbad83616393616808567bf28d9d5c0465b
AKASHI Takahiro <takahiro.akashi@linaro.org>
2020-10-29 09:25:27 +0900

    test/py: efi_capsule: test for FIT image capsule
    The test can run on sandbox build and it attempts to execute a firmware
    update via a capsule-on-disk, using a FIT image capsule,
    CONFIG_EFI_CAPSULE_FIT.
    
    To run this test successfully, you need configure U-Boot specifically;
    See test_capsule_firmware.py for requirements, and hence it won't run
    on Travis CI, at least, for now.
    
    Signed-off-by: AKASHI Takahiro <takahiro.akashi@linaro.org>

---
+++ test/py/tests/test_efi_capsule/defs.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier:      GPL-2.0+
+
+# Size in MiB
+EFI_BOOTDEV_IMAGE_SIZE = 16
+EFI_BOOTDEV_PART_SIZE = 8
+
+# Owner guid
+GUID = '11111111-2222-3333-4444-123456789abc'
+
+# Directories
+CAPSULE_DATA_DIR = '/EFI/CapsuleTestData'
+CAPSULE_INSTALL_DIR = '/EFI/UpdateCapsule'
