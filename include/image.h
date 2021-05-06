2f921cf0973474402684c4559740d28ccf5d61bc
Simon Glass <sjg@chromium.org>
2021-05-06 08:24:20 -0600

    image: Drop IMAGE_ENABLE_SHAxxx
    We already have a host Kconfig for these SHA options. Use
    CONFIG_IS_ENABLED(SHAxxx) directly in the code shared with the host build,
    so we can drop the unnecessary indirections.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/image.h
+++ include/image.h
@@ -47,27 +47,6 @@ struct fdt_region;
 #include <linux/libfdt.h>
 #include <fdt_support.h>
 
-#if defined(CONFIG_FIT_SHA256) || \
-	defined(CONFIG_SPL_FIT_SHA256)
-#define IMAGE_ENABLE_SHA256	1
-#else
-#define IMAGE_ENABLE_SHA256	0
-#endif
-
-#if defined(CONFIG_FIT_SHA384) || \
-	defined(CONFIG_SPL_FIT_SHA384)
-#define IMAGE_ENABLE_SHA384	1
-#else
-#define IMAGE_ENABLE_SHA384	0
-#endif
-
-#if defined(CONFIG_FIT_SHA512) || \
-	defined(CONFIG_SPL_FIT_SHA512)
-#define IMAGE_ENABLE_SHA512	1
-#else
-#define IMAGE_ENABLE_SHA512	0
-#endif
-
 #endif /* FIT */
 
 #ifdef CONFIG_SYS_BOOT_GET_CMDLINE
