04f9a0f2bfcf9cdf7eb2c028761e9104543e0494
Simon Glass <sjg@chromium.org>
2021-08-08 12:20:30 -0600

    net: Rename SPL_NET_SUPPORT to SPL_NET
    Rename this option so that CONFIG_IS_ENABLED can be used with it.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- lib/tiny-printf.c
+++ lib/tiny-printf.c
@@ -48,7 +48,7 @@ static void div_out(struct printf_info *info, unsigned long *num,
 		out_dgt(info, dgt);
 }
 
-#ifdef CONFIG_SPL_NET_SUPPORT
+#ifdef CONFIG_SPL_NET
 static void string(struct printf_info *info, char *s)
 {
 	char ch;
@@ -178,7 +178,7 @@ static void __maybe_unused pointer(struct printf_info *info, const char *fmt,
 		}
 		break;
 #endif
-#ifdef CONFIG_SPL_NET_SUPPORT
+#ifdef CONFIG_SPL_NET
 	case 'm':
 		return mac_address_string(info, ptr, false);
 	case 'M':
