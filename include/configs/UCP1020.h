14ea1cf74162818f68f1530ec65bd90cf66bf8f1
Tom Rini <trini@konsulko.com>
2021-09-13 15:08:00 -0400

    kgdb: Remove unused serial related options
    We have a few CONFIG options for KGDB that are not referenced, remove
    them.
    
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- include/configs/UCP1020.h
+++ include/configs/UCP1020.h
@@ -386,11 +386,6 @@
 #define CONFIG_SYS_BOOTMAPSZ	(64 << 20)	/* Initial Memory for Linux*/
 #define CONFIG_SYS_BOOTM_LEN	(64 << 20)	/* Increase max gunzip size */
 
-#if defined(CONFIG_CMD_KGDB)
-#define CONFIG_KGDB_BAUDRATE	230400	/* speed to run kgdb serial port */
-#define CONFIG_KGDB_SER_INDEX	2	/* which serial port to use */
-#endif
-
 /*
  * Environment Configuration
  */
