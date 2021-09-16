9c531456e30c8a1fcd16b8d72ff45bc4444001d0
Chia-Wei Wang <chiawei_wang@aspeedtech.com>
2021-09-16 16:52:20 +0800

    configs: aspeed: Make EXTRA_ENV_SETTINGS board specific
    Move CONFIG_EXTRA_ENV_SETTINGS to board-specific
    configuration headers.
    
    Signed-off-by: Chia-Wei Wang <chiawei_wang@aspeedtech.com>

--- include/configs/evb_ast2600.h
+++ include/configs/evb_ast2600.h
@@ -10,4 +10,13 @@
 
 #define CONFIG_SYS_UBOOT_BASE		CONFIG_SYS_TEXT_BASE
 
+/* Memory Info */
+#define CONFIG_SYS_LOAD_ADDR		0x83000000
+
+/* Misc */
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"verify=yes\0"	\
+	"spi_dma=yes\0" \
+	""
+
 #endif	/* __CONFIG_H */
