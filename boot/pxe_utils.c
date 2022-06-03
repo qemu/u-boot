qemu/u-boot

commit d5ba6188dfbf6bb68354bec86e483623f1f6dae2

cmd: pxe_utils: Check fdtcontroladdr in label_boot
If using OF_CONTROL, fdtcontroladdr is set to the fdt used to configure
U-Boot. When using PXE, if no fdt is defined in the menu file, and
there is no fdt at fdt_addr, add fall back on fdtcontroladdr too.

We are developing board support for the Armv8r64 FVP using
config_distro_bootcmd. We are also using OF_BOARD and would like the
PXE boot option to default to the fdt provided by board_fdt_blob_setup.

Signed-off-by: Peter Hoyes <Peter.Hoyes@arm.com>
Reviewed-by: Simon Glass <sjg@chromium.org>
Signed-off-by: Ramon Fried <rfried.dev@gmail.com>

boot/pxe_utils.c
@@ -550,7 +550,10 @@ static int label_boot(struct pxe_context *ctx, struct pxe_label *label)
 	 * Scenario 2: If there is an fdt_addr specified, pass it along to
 	 * bootm, and adjust argc appropriately.
 	 *
-	 * Scenario 3: fdt blob is not available.
+	 * Scenario 3: If there is an fdtcontroladdr specified, pass it along to
+	 * bootm, and adjust argc appropriately.
+	 *
+	 * Scenario 4: fdt blob is not available.
 	 */
 	bootm_argv[3] = env_get("fdt_addr_r");
 
@@ -651,6 +654,9 @@ static int label_boot(struct pxe_context *ctx, struct pxe_label *label)
 
 	if (!bootm_argv[3])
 		bootm_argv[3] = env_get("fdt_addr");
+
+	if (!bootm_argv[3])
+		bootm_argv[3] = env_get("fdtcontroladdr");
 
 	if (bootm_argv[3]) {
 		if (!bootm_argv[2])

drivers/net/tsec.c
@@ -156,7 +156,7 @@ static int tsec_mcast_addr(struct udevice *dev, const u8 *mcast_mac, int join)
 	return 0;
 }
 
-static int tsec_set_promisc(struct udevice *dev, bool enable)
+static int __maybe_unused tsec_set_promisc(struct udevice *dev, bool enable)
 {
 	struct tsec_private *priv = dev_get_priv(dev);
 	struct tsec __iomem *regs = priv->regs;

commit f68087a465d5c2fa61fa4b6c6f0980fc157fb547

boot: pxe_utils: Do not use fdtcontroladdr for FIT Image format
When patch [1] introduced "fdtcontroladdr" fallback in order to use
built-in DT when no DT in provided in PXE config, it broke FIT image
boot where the DT from the FIT configuration was used.

This disables the "fdtcontroladdr" fallback when the provided
image is from the FIT image type.

[1] d5ba6188dfbf ("cmd: pxe_utils: Check fdtcontroladdr in label_boot")

Fixes: d5ba6188dfbf ("cmd: pxe_utils: Check fdtcontroladdr in label_boot")
Cc: Tom Rini <trini@konsulko.com>
Cc: Peter Hoyes <peter.hoyes@arm.com>
Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>

boot/pxe_utils.c
@@ -725,7 +725,7 @@ static int label_boot(struct pxe_context *ctx, struct pxe_label *label)
 	if (!bootm_argv[3])
 		bootm_argv[3] = env_get("fdt_addr");
 
-	if (!bootm_argv[3])
+	if (genimg_get_format(buf) != IMAGE_FORMAT_FIT && !bootm_argv[3])
 		bootm_argv[3] = env_get("fdtcontroladdr");
 
 	if (bootm_argv[3]) {