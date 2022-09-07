c4b45206ee65123864b4eb5225b9c2ca135c2335
Simon Glass <sjg@chromium.org>
2022-09-06 20:27:18 -0600

    dm: core: Expand integer-reading tests
    The current tests do not cover all the behaviour. Add some more.
    
    Tidy up a few inconsistencies between livetree and flattree which come to
    light with these tests. Also drop the -ENODATA error since it is never
    actually returned.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- drivers/core/ofnode.c
+++ drivers/core/ofnode.c
@@ -296,9 +296,16 @@ int ofnode_read_u32_array(ofnode node, const char *propname,
 		return of_read_u32_array(ofnode_to_np(node), propname,
 					 out_values, sz);
 	} else {
-		return fdtdec_get_int_array(gd->fdt_blob,
-					    ofnode_to_offset(node), propname,
-					    out_values, sz);
+		int ret;
+
+		ret = fdtdec_get_int_array(gd->fdt_blob,
+					   ofnode_to_offset(node), propname,
+					   out_values, sz);
+		if (ret == -FDT_ERR_NOTFOUND)
+			return -EINVAL;
+		else if (ret == -FDT_ERR_BADLAYOUT)
+			return -EOVERFLOW;
+		return ret;
 	}
 }
 
