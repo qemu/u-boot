14beaada9f8c577eb4f4ba652eeee921dde17ea8
Hari Nagalla <hnagalla@ti.com>
2022-11-19 12:59:24 -0600

    arm: K3: Add basic support for J784S4 SoC definition
    Add basic support for J784S4 SoC definition
    
    Signed-off-by: Hari Nagalla <hnagalla@ti.com>
    Signed-off-by: Apurva Nandan <a-nandan@ti.com>
    Signed-off-by: Bryan Bratloff <bb@ti.com>
    Signed-off-by: Nishant Menon <nm@ti.com>

--- arch/arm/mach-k3/arm64-mmu.c
+++ arch/arm/mach-k3/arm64-mmu.c
@@ -262,3 +262,44 @@ struct mm_region am64_mem_map[NR_MMU_REGIONS] = {
 
 struct mm_region *mem_map = am64_mem_map;
 #endif /* CONFIG_SOC_K3_AM642 || CONFIG_SOC_K3_AM625 */
+
+#ifdef CONFIG_SOC_K3_J784S4
+#define NR_MMU_REGIONS	(CONFIG_NR_DRAM_BANKS + 3)
+
+/* ToDo: Add 64bit IO */
+struct mm_region j784s4_mem_map[NR_MMU_REGIONS] = {
+	{
+		.virt = 0x0UL,
+		.phys = 0x0UL,
+		.size = 0x80000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
+			 PTE_BLOCK_NON_SHARE |
+			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
+	}, {
+		.virt = 0x80000000UL,
+		.phys = 0x80000000UL,
+		.size = 0x80000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
+			 PTE_BLOCK_INNER_SHARE
+	}, {
+		.virt = 0x880000000UL,
+		.phys = 0x880000000UL,
+		.size = 0x80000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
+			 PTE_BLOCK_INNER_SHARE
+	}, {
+		.virt = 0x500000000UL,
+		.phys = 0x500000000UL,
+		.size = 0x400000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
+			 PTE_BLOCK_NON_SHARE |
+			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
+	}, {
+		/* List terminator */
+		0,
+	}
+};
+
+struct mm_region *mem_map = j784s4_mem_map;
+
+#endif /* CONFIG_SOC_K3_J784S4 */
